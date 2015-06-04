f27bfc0128e679ab7ce88699b6f7d109c4baf044
mathinvalidnik <kazmanov@uni-sofia.bg>
2015-06-04 17:47:44 +0300

    User authentication
    User authentication was added to the project

--- TMan/TMan/LoginForm.cs
+++ TMan/TMan/LoginForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TMan;
+using TMan.Helpers;
 
 namespace TMan
 {
@@ -22,26 +23,38 @@ namespace TMan
         {
             try
             {
-                //Ако потребителят съществува в базата и е администратор -> Зареди екрана за администратори
-                //Ако потребителят съществува в базата и не е администра -> Зареди екрана за обикновени потр.
-                //Ако потребителят не съществува -> MessageBox.Show("User doesn't exist");
-                this.Hide();
-                if (tbUsernameInput.Text == "holla" && tbPasswordInput.Text == "123")
+                
+                var entities = new TManDBEntities();
+                var userIsAvailableInDatabase = (from el in entities.TMUsers
+                                                where el.Username == tbUsernameInput.Text && el.Password == tbPasswordInput.Text
+                                                select el).Any();
+
+                var userIsAdministrator = userIsAvailableInDatabase && (from el in entities.TMUsers
+                                                                        where   el.Username == tbUsernameInput.Text 
+                                                                            &&  el.Password == tbPasswordInput.Text
+                                                                            &&  el.Role == "Administrator"
+                                                                        select el).Any();
+
+                if (userIsAvailableInDatabase && userIsAdministrator)
                 {
-                    AdministratorDialog adminDialog = new AdministratorDialog();
+                    this.Hide();
+                    var adminDialog = new AdministratorDialog();
                     adminDialog.ShowDialog();
                 }
-                else
+                else if (userIsAvailableInDatabase && !userIsAdministrator)
                 {
+                    this.Hide();
                     RegularUserDialog regularDialog = new RegularUserDialog();
                     regularDialog.ShowDialog();
                 }
-
-                
+                else
+                {
+                    MessageBox.Show("User is not available in the database! Try with another one or different credentials.", "Message");
+                }
             }
             catch(Exception ex)
             {
-                MessageBox.Show("There was a fatal error while trying to log you in. See more:\n", ex.Message);
+                MessageBox.Show(string.Format("There was a fatal error while trying to log you in. See more:\n{0}", ex.Message));
             }
         }
     }
